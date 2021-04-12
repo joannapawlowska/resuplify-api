52a8cfea9792af74d768484cff03ff75984b77be
Joanna Pawlowska <pawlo.joan@gmail.com>
2021-04-13 00:43:54 +0200

    Initial project upload

---
+++ src/main/java/com/io/resuplifyapi/ResuplifyApiApplication.java
@@ -0,0 +1,12 @@
+package com.io.resuplifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResuplifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResuplifyApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/io/resuplifyapi/ResuplifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.resuplifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResuplifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
