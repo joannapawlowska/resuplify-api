7d4ed4cb4a2a9222ab2e92c1e80c0bfdf2e59f59
Joanna Pawlowska <pawlo.joan@gmail.com>
2021-05-23 23:48:50 +0200

    Implement UserDetails and its service

--- src/main/java/com/io/resuplifyapi/domain/User.java
+++ src/main/java/com/io/resuplifyapi/domain/User.java
@@ -1,10 +1,14 @@
 package com.io.resuplifyapi.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name="user_account")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -59,5 +63,30 @@ public class User {
 
     public void setShop(Shop shop) {
         this.shop = shop;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 }
--- src/main/java/com/io/resuplifyapi/service/UserService.java
+++ src/main/java/com/io/resuplifyapi/service/UserService.java
@@ -5,39 +5,60 @@ import com.io.resuplifyapi.domain.User;
 import com.io.resuplifyapi.domain.UserDto;
 import com.io.resuplifyapi.domain.externalAPI.AuthResponse;
 import com.io.resuplifyapi.exception.UserAlreadyExistException;
+import com.io.resuplifyapi.exception.UserNotFoundException;
 import com.io.resuplifyapi.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
-    private UserRepository userAccountRepository;
+    private UserRepository userRepository;
 
-    public Optional<User> findById(int id){
-        return userAccountRepository.findById(id);
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void save(User user){
-        userAccountRepository.save(user);
+        userRepository.save(user);
    }
 
     public void createNewUserAccount(UserDto userDto, AuthResponse response) throws UserAlreadyExistException {
 
-        if(existByUsernameAndShopUrl(userDto.getUsername(), userDto.getUrl()))
+        if(existByUsername(userDto.getUsername()))
             throw new UserAlreadyExistException(userDto.getUsername(),  userDto.getUrl());
 
-        Shop shop = new Shop(userDto.getUrl(), response.getAccessToken(), LocalDate.now());
-        User user = new User(userDto.getUsername(), userDto.getPassword(), shop);
+        Shop shop = new Shop(userDto.getUrl(), response.getAccessToken(), LocalDate.now().plusDays(TimeUnit.SECONDS.toDays(response.getExpiresIn())));
+        User user = new User(userDto.getUsername(), passwordEncoder.encode(userDto.getPassword()), shop);
         save(user);
     }
 
-    public boolean existByUsernameAndShopUrl(String username, String url){ return userAccountRepository.existsByUsernameAndShopUrl(username, url); }
+    public boolean existByUsername(String username){ return userRepository.existsByUsername(username); }
+
+    public User findByUsername(String username) throws UserNotFoundException {
+
+        if(!existByUsername(username))
+            throw new UserNotFoundException(username);
+
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        try{
+            return findByUsername(s);
+        }catch(UserNotFoundException e){
+            throw new UsernameNotFoundException(e.getMessage());
+        }
+    }
 }
